d8a7484e9181ebff6891b417c3b0669f6852688a
Bryan Galindo <galindo.bryan08@gmail.com>
2025-01-28 11:19:26 -0600

    add dynamic log leveling for non prod env

--- recruiterblast/logger.py
+++ recruiterblast/logger.py
@@ -1,11 +1,13 @@
 import logging
+import recruiterblast.config as cfg
 
 
 def setup_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
+    log_level = logging.DEBUG if cfg.ENV != "prod" else logging.INFO
+    console_handler.setLevel(log_level)
     formatter = logging.Formatter(
         "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     )
