fe0509892c441e50ea3590c8a1d1a56b846f683e
Bryan Galindo <galindo.bryan08@gmail.com>
2025-01-27 19:36:05 -0600

    add bing api env key

--- recruiterblast/config.py
+++ recruiterblast/config.py
@@ -6,4 +6,7 @@ load_dotenv()
 
 LINKEDIN_CSRF_TOKEN = os.environ["LINKEDIN_CSRF_TOKEN"]
 LINKEDIN_COOKIE = os.environ["LINKEDIN_COOKIE"]
+
+BING_SEARCH_API_KEY = os.getenv("BING_SEARCH_API_KEY")
+
 ENV = os.environ["ENV"]
