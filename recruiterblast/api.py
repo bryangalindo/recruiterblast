288a02cfff2849114a5428b7f59ce28712772cf1
Bryan Galindo <galindo.bryan08@gmail.com>
2025-01-29 18:28:01 -0600

    update prompt to fix failing llm skill parsing

--- recruiterblast/api.py
+++ recruiterblast/api.py
@@ -20,27 +20,36 @@ class GoogleGeminiAPIClient:
 
     def parse_skills_from_job_description(self, job_description: str) -> dict:
         prompt = (
-            "Parse this job description for technology keywords and soft skill keywords. "
-            "Return only a JSON dictionary in the exact format shown below. No explanations, no extra text, and no context beyond the JSON output.\n\n"
-            '{\n  "Technologies": [\n    "SQL", "Python", "Typescript", "dbt", "AWS", "GCP", "Snowflake",\n'
-            '    "QuickBase", "Google Analytics", "API", "data orchestration"\n  ]\n}\n\n'
-            f"Job description:\n{job_description}"
+            "Parse this job description for technology keywords. "
+            "Return only a JSON dictionary string in the exact format shown below. "
+            "No explanations, no extra text, no formatting (e.g., 'json\n{'technologies': ['Python']}\n\n',), "
+            ", no new line characters (e.g., '{'technologies': ['Python']}\n'), and no context beyond the JSON output. "
+            '"{"technologies": ["SQL", "Python", "Typescript", "Snowflake", "API"]}"'
+            f"Job description: {job_description}"
         )
         payload = {"contents": [{"parts": [{"text": prompt}]}]}
+        log.debug(f"Starting to submit {prompt=}...")
         response = self._make_request(payload)
         parser = GoogleGeminiAPIResponseParser(response)
         text = parser.get_response_text()
         return safe_parse_dict_from_json_str(text)
 
     def _make_request(self, payload: dict) -> dict:
-        """
-        Send a POST request to the Gemini API with the given payload.
-        """
         url = f"{self.BASE_URL}?key={self.api_key}"
         response = requests.post(url, headers=self.headers, json=payload)
 
         if response.status_code == 200:
+            log.info(f"Successfully received response from {url=}")
             return response.json()
         else:
-            log.info(f"Failed to get response from ")
+            log.warning(
+                f"Failed to get response from {url=}, {response.status_code}, "
+                f"{response.reason}, {payload=}"
+            )
             return {}
+
+
+if __name__ == "__main__":
+    client = GoogleGeminiAPIClient()
+    skills = client.parse_skills_from_job_description("Python job for money.")
+    print(skills)
