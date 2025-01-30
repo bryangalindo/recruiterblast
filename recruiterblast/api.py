2776a1f1640f81c1f83dbdd64b9a7fe22b47c8c1
Bryan Galindo <galindo.bryan08@gmail.com>
2025-01-29 19:21:46 -0600

    update global var to add context

--- recruiterblast/api.py
+++ recruiterblast/api.py
@@ -2,6 +2,7 @@ import requests
 
 import recruiterblast.config as cfg
 from recruiterblast.constants import GOOGLE_GEMINI_API_URL
+from recruiterblast.prompts import LLM_JOB_DESCRIPTION_SKILLS_PARSER_PROMPT
 from recruiterblast.logger import setup_logger
 from recruiterblast.parsers import (
     GoogleGeminiAPIResponseParser,
@@ -19,16 +20,8 @@ class GoogleGeminiAPIClient:
         self.headers = {"Content-Type": "application/json"}
 
     def parse_skills_from_job_description(self, job_description: str) -> dict:
-        prompt = (
-            "Parse this job description for technology keywords. "
-            "Return only a JSON dictionary string in the exact format shown below. "
-            "No explanations, no extra text, no formatting (e.g., 'json\n...\n\n',), "
-            ", no new line characters (e.g., '\n'), and no context beyond the JSON output. "
-            "This is an example of what the output should be: "
-            '"{"technologies": ["SQL", "Python", "Typescript", "Snowflake", "API", "dbt", "AWS", "GCP", "Snowflake",'
-            '"QuickBase", "Google Analytics", "API", "data orchestration"]}"'
-            f"Job description: {job_description}"
-        )
+        prompt = LLM_JOB_DESCRIPTION_SKILLS_PARSER_PROMPT
+        prompt += f"Job description: {job_description}"
         payload = {"contents": [{"parts": [{"text": prompt}]}]}
         log.debug(f"Starting to submit {prompt=}...")
         response = self._make_request(payload)
@@ -53,7 +46,16 @@ class GoogleGeminiAPIClient:
 
 if __name__ == "__main__":
     pass
-    # description = ""
+    # description = """
+    # 2+ years of professional experience in software development, with a strong emphasis on development of large-scale and performant applications.
+    # Knowledge in at least one server-side language such as Java, C++, C#, Go, Python, Kotlin, or Ruby.
+    # Ability towards building resilient and operationally efficient backend systems exemplifying industry standards (HTTP REST, GraphQL, Serverless, SOA, etc).
+    # A desire to ship world class code daily.
+    # Good attention to detail, as well as a passion for delivering high-quality software solutions and compelling customer experiences
+    # Good problem-solving skills, possessing the ability to work independently or collaboratively to analyze and address complex technical challenges
+    # Excellent communication skills with the ability to collaborate effectively with cross-functional teams and those in non-technical roles, such as marketing, finance, and legal
+    # Knowledge of modern software development practices, version control systems, and agile methodologies
+    # """
     # client = GoogleGeminiAPIClient()
     # skills = client.parse_skills_from_job_description(description)
     # print(skills)
